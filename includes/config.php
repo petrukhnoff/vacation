bf6916b25b32838ff3d6efa4747c82d311468f32
petrukhnoff <petrukhnof@gmail.com>
2018-02-05 12:35:22 +0200

    first commit

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,6 @@
-
 <?php
+
 define('DB_SERVER', 'localhost');
 define('DB_SERVER_USERNAME', 'root');
 define('DB_SERVER_PASSWORD', '');
 define('DB_DATABASE', 'vacation');
-
-echo 'gfgfgjjj   ';
-$t = DB_DATABASE;
-print_r($t);
-//var_dump($t);
