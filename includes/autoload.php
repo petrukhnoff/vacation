920559d6806e9563021a1313fa24cffa8214c6c3
petrukhnoff <petrukhnof@gmail.com>
2018-02-05 13:03:48 +0200

    first commit

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,6 +1,5 @@
 <?php
 spl_autoload_register(function ($class_name) {
-  var_dump($class_name);
   switch (true) {
     case file_exists('includes/classes/' . $class_name . '.php'):
       $path = 'includes/classes/' . $class_name . '.php';
